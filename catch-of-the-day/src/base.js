091eca46fac60cdf36d8f0907a2af0bcd4f225e5
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-03-19 12:03:54 -0500

    Finished 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBd7PreOdUY1Hs0BSZLVAuTWRFTBqJmt7A",
+  authDomain: "catch-of-the-day-tammy-r.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-tammy-r.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,34 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes'
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+
     // getinitialstate
     this.state = {
       fishes: {},
       order: {}
     }
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
@@ -22,7 +39,22 @@ class App extends React.Component {
     const timestamp = Date.now();
     fishes[`fish-${timestamp}`] = fish;
     // set state
-    this.setState({ fishes })
+    this.setState({ fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
   }
 
   render() {
@@ -30,9 +62,15 @@ class App extends React.Component {
         <div className="catch-of-the-day">
           <div className="menu">
             <Header tagline="Fresh Seafood Market" />
+            <ul className="list-of-fishes">
+              {Object
+                .keys(this.state.fishes) // creates an array
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />) // this loops over the array and returns each Fish key
+              }
+            </ul>
           </div>
-          <Order />
-          <Inventory addFish={this.addFish} />
+          <Order fishes={this.state.fishes} order={this.state.order} />
+          <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
         </div>
     )
   }
